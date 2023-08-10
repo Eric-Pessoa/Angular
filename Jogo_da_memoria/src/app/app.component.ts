384cbcf332741bcd22ff4abc90bdaea1a1c77cd7
EricP <pessoa.eric@gmail.com>
2023-08-10 12:32:35 -0300

    Implemented randomness and new images

--- Jogo_da_memoria/src/app/app.component.ts
+++ Jogo_da_memoria/src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Jogo_da_memoria';
+  idNumbers: number[] = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
   ngOnInit() {
+    this.defineAnimation()
+    this.shuffleArrayOfIdsOrder(this.idNumbers)
+  }
+  defineAnimation(): void {
     const cards = document.getElementsByClassName("flip-card");
     for (let index = 0; index < cards.length; index++) {
       const card = cards[index];
@@ -20,6 +25,14 @@ export class AppComponent {
           innerCard.classList.remove('flipBack')
         }
       })
+    }
+  }
+   shuffleArrayOfIdsOrder(array: number[]) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
   }
 }
--- Jogo_da_memoria/src/app/components/Card/card.component.ts
+++ Jogo_da_memoria/src/app/components/Card/card.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
 })
-export class CardComponent {
+export class CardComponent  {
+
+  @Input() itemId = 0; 
+  imgSrc = '';
+
+  ngOnInit() {
+    this.defineImage();
+  }
+
+  defineImage() {
+    switch(this.itemId) {
+      case 1:
+        this.imgSrc = '../../../assets/images/shapes/glass_losangle.png'
+        break;
+      case 2:
+        this.imgSrc = '../../../assets/images/shapes/crescent_moon.png'
+        break;
+      case 3:
+        this.imgSrc = '../../../assets/images/shapes/orange_half_circle.png'
+        break;
+      case 4:
+        this.imgSrc = '../../../assets/images/shapes/purple_circle.png'
+        break;
+      case 5:
+        this.imgSrc = '../../../assets/images/shapes/red_triangle.png'
+        break;
+      case 6:
+        this.imgSrc = '../../../assets/images/shapes/yellow_square.png'
+        break;
+    }
+  }
+
+  
 
 }
