1002418b00dc1c3d8027b2a2c64e3648f24df744
EricP <pessoa.eric@gmail.com>
2023-08-18 12:09:40 -0300

    More project configuration

--- Dicionario/src/app/app.module.ts
+++ Dicionario/src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { InputTextComponent } from './components/input-text/input-text.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputTextComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatInputModule
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
