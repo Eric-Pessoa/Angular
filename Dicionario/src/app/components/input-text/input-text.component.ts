6d94993aeb1effbebf3fe7f572fd064e01a57476
EricP <pessoa.eric@gmail.com>
2023-08-18 15:21:55 -0300

    progresso

--- Dicionario/src/app/components/input-text/input-text.component.ts
+++ Dicionario/src/app/components/input-text/input-text.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-text',
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-text.component.scss']
 })
 export class InputTextComponent {
+
+  @Input() inputValue = ''
+  @Output() inputEmission =  new EventEmitter<string>(); 
+
+ emitValue(e: any ) {
+  this.inputEmission.emit(e)
+ }
 
  texto = ''
 
